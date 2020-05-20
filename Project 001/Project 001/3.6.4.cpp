da40d5dd101816d7c548f7bea579606a48447720
eliezerseror <62102702+eliezerseror@users.noreply.github.com>
2020-05-21 00:42:42 +0300

    update 21.05

--- Project 001/Project 001/3.6.4.cpp
+++ Project 001/Project 001/3.6.4.cpp
@@ -26,56 +26,32 @@ int main()
 		year_div_4, year_p_div_4, month_code, month_print, day_code, day_in_week;
 
 	cout << "Please enter the date you like: xx/xx/xx\n";
-	cin >> day >> month >> year;
-	if (year <= 3000 && month <= 12)
+	cin >> month;
+
+	bool status1, status2, status3, status4;
+	
+	status1 = status2 = status3 = status4 = false;
+
+	if (month == 1)
 	{
-		if (((month = 1, 3, 5, 7, 8, 10, 12 && day <= 31) || (month = 4, 6, 9, 11 && day <= 30)) || ((month = 2 && (year % 4) == 0) && (day <= 29)) || (((year % 4) == 0) && (day <= 29)))
-
-			year_div_4 = year / 4;
-		year_p_div_4 = year_div_4 + year;
-		month_code = 0;
-
-		switch (month)
-		{
-		case 1: month_code = 1;
-			break;
-		case 2: month_code = 4;
-			break;
-		case 3: month_code = 4;
-			break;
-		case 4: month_code = 0;
-			break;
-		case 5: month_code = 2;
-			break;
-		case 6: month_code = 5;
-			break;
-		case 7: month_code = 0;
-			break;
-		case 8: month_code = 3;
-			break;
-		case 9: month_code = 6;
-			break;
-		case 10: month_code = 1;
-			break;
-		case 11: month_code = 4;
-			break;
-		case 12: month_code = 6;
-			break;
-		}
-
-		day_code = day + year_p_div_4 + month_code;
-		day_in_week = day_code % 7;
-
-		if (day_in_week == 0) cout << "The date you enter is saturday";
-		if (day_in_week == 1) cout << "The date you enter is sunday";
-		if (day_in_week == 2) cout << "The date you enter is munday";
-		if (day_in_week == 3) cout << "The date you enter is tuesday";
-		if (day_in_week == 4) cout << "The date you enter is wednesday";
-		if (day_in_week == 5) cout << "The date you enter is thursday";
-		if (day_in_week == 6) cout << "The date you enter is friday";
-
+		status1 = true;
 	}
-	else cout << "Wrong year";
+	else 
+		if (month == 2)
+		{
+			status2 = true;
+		}
+		else
+			if (month == 3)
+			{
+				status3 = true;
+			}
+			else
+				if (month == 4)
+				{
+					status4 = true;
+				}
 
-	return 0;
+	cout << status1 << endl << status2 << endl << status3 << endl << status4;
+		return 0;
 }
