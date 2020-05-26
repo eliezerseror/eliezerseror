d6ee9f95dfea3cf55ef30af1ef331e2b527ab4fe
eliezerseror <eliezerseror@gmail.com>
2020-05-26 17:29:15 +0300

    update

---
+++ Project 001/Project 001/4.1.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+	int num, row;
+
+	num = 0;
+
+		while (num < 100) {
+			cout << num;
+			if (num % 10 == 0)
+				cout << endl;
+			num = num +2;
+
+	}
+
+
+
+
+
+	return 0;
+}
